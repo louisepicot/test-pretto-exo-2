69b33ade316b97a39297e9933a8f3e8ca6b3d863
Louise Picot <louise.picot@needelp.com>
2026-01-13 22:29:22 +0100

    feat: setup base app structure and styling

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./index.css";
+import { Header } from "./components/Header";
+import { AlertForm } from "./components/AlertForm";
+import { AlertFormProvider } from "./contexts";
+
+function App() {
+  return (
+    <AlertFormProvider>
+      <Header />
+      <AlertForm />
+    </AlertFormProvider>
+  );
+}
+
+export default App;
